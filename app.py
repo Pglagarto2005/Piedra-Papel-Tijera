7b6ec7ab299c0bdcc8bf469295d482d2d0bd661a
Pglagarto2005 <santiblo05@gmail.com>
2026-03-19 10:52:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,13 @@ if img_file_buffer is not None:
     prediction = model.predict(data)
     print(prediction)
     if prediction[0][0]>0.5:
-      st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
+      st.header('Piedra, con Probabilidad: '+str( prediction[0][0]) )
     if prediction[0][1]>0.5:
-      st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
+      st.header('Papel, con Probabilidad: '+str( prediction[0][1]))
+    if prediction[0][0]>0.5:
+      st.header('Tijera, con Probabilidad: '+str( prediction[0][2]) )
+    if prediction[0][0]>0.5:
+      st.header('Nada, con Probabilidad: '+str( prediction[0][3]) )
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
